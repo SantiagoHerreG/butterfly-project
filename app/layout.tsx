9f76de3a3cbc294349073b505f30820a97a5350c
Santiago Herrera <sherrera-ext@alyce.com>
2023-01-29 21:22:49 -0500

    Set up Nextjs and firts page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
-import './globals.css'
+import "./globals.css";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -12,7 +13,16 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <header>
+          <Image
+            src="https://assets.butterfly.ai/email-images/bf-banner.v2.png"
+            fill
+            alt="logo"
+          />
+        </header>
+        <main>{children}</main>
+      </body>
     </html>
-  )
+  );
 }
